7edbd322d17972b18ad243c7278fc6c4772cebda
Jesse Brown <brownjesse8895@yahoo.com>
2021-02-19 16:24:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value)|
     value.each do |inner_key, names|
-      if final_array[name]
-        final_array[name] = {}
+      final_array[names] = {} 
       end 
   binding.pry
 end 
