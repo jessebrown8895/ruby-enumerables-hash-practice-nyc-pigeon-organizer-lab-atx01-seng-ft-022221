1edcd7e3c781ae86faf4fc19a656784337494072
Jesse Brown <brownjesse8895@yahoo.com>
2021-02-19 16:07:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |key, value|
     key.each do |inner_key, names|
       binding.pry
-      inner_key.each do |names|
+      names.each do |names|
         binding.pry
     end 
       binding.pry
