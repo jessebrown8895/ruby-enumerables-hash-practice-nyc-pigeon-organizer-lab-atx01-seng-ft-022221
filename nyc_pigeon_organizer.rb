6a74b38034e1c5c63f609407918d7d83956890e9
Jesse Brown <brownjesse8895@yahoo.com>
2021-02-19 16:18:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
-def nyc_pigeon_organizer 
-  data.each_with_object({}) do |(key, value) |
+def nyc_pigeon_organizer(data)
+  data.each_with_object({}) do |(key, value)|
   binding.pry
 end 
 end 
